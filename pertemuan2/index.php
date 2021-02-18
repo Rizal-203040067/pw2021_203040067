6c2ba06a79c4e3e96e608a785e640fd574a255d1
Rizal-203040067 <rizal.baihaqi007@gmail.com>
2021-02-18 22:11:43 +0700

    menyelesaikan pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,65 @@
+<?php
+/*
+Rizal Baihaqi
+203040067
+https://github.com/Rizal-203040067/pw2021_203040067.git
+Pertemuan 1 - 18 Februari 2021
+Mempelajari mengenai sintaks PHP
+*/
+?>
+<?php 
+// pertemuan 2 - PHP Dasar
+// Sintaks PHP 
+
+// Standar Output
+// echo, print
+// Print_r
+// var_dump
+
+// Penulisan Sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variabel dan Tipe Data
+// Variabel
+// tidak boleh diawali dengan angka, tapi boleh mengandung angka
+// $nama = "Rizal Baihaqi";
+// echo 'Halo, nama saya $nama';
+
+// Operator
+// arimatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// penggabung string / concatenation / concat
+// .
+// $nama_depan = "Rizal";
+// $nama_belakang = "Baihaqi";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assignment
+// =, +=, -=, *=, /=, %=, .=
+// $x =1;
+// $x -= 5;
+// echo $x;
+// $nama = "Rizal";
+// $nama .= " ";
+// $nama .= "Baihaqi";
+// echo $nama;
+
+// Perbandingan
+// <, >, <=, >=, ==, !=
+// var_dump(1 == "1");
+
+// identitas
+// ===, !==
+// var_dump(1 === "1");
+
+// Logika 
+// &&, ||, ! 
+// $x = 10;
+// var_dump($x < 20 || $x % 2 == 0);
+
+?>
