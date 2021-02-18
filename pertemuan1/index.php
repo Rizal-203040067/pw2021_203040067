be640ccfb098cf253d104773c713a02aff9ff919
Rizal-203040067 <rizal.baihaqi007@gmail.com>
2021-02-18 21:43:57 +0700

    menyelesaikan pertemuan 1

---
+++ pertemuan1/index.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Rizal Baihaqi
+203040067
+https://github.com/Rizal-203040067/pw2021_203040067.git
+Pertemuan 1 - 5 Februari 2021
+Mempelajari mengenai dasar PHP
+*/
+?>
+<?php
+    echo "Hello World!";
+?>
