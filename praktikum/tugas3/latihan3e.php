4f319a96cc7ac3ae698dcecf4190b44bdec6a16b
Rizal-203040067 <rizal.baihaqi007@gmail.com>
2021-03-18 10:30:20 +0700

    revisi praktikum pertemuan 3

--- praktikum/tugas3/latihan3e.php
+++ praktikum/tugas3/latihan3e.php
@@ -29,7 +29,7 @@ Modul 3 Array
 
     <title>Code Gaming</title>
   </head>
-  <body>
+  <body class="bg-dark">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -55,7 +55,7 @@ Modul 3 Array
     </div>
     </nav>
 
-    <table class="table table-dark table-striped">
+    <table class="table table-dark table-hover table-striped">
         <thead>
             <tr>
                 <th></th>
