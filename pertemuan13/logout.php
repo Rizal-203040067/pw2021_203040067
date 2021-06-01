939ec6e2aa234add69df948feb57372961988f9f
Rizal-203040067 <rizal.baihaqi007@gmail.com>
2021-06-02 00:34:04 +0700

    menyelesaikan pertemuan 13

---
+++ pertemuan13/logout.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Rizal Baihaqi
+203040067
+https://github.com/Rizal-203040067/pw2021_203040067.git
+Pertemuan 13- 1 Juni 2021
+Mempelajari mengenai AJAX live Search dan Upload Gambar
+*/
+?>
+
+<?php
+session_start();
+session_destroy();
+header('Location: login.php');
+exit;
+?>
